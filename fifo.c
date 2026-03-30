7f4688b201679e831edb57b44c09c792e13050c2
sreelakshmip2005 <sreelakshmip6122005@gmail.com>
2026-03-30 21:16:38 +0530

    fifo.c

---
+++ fifo.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) {
+    int n = atoi(argv[1]), f = atoi(argv[2]);
+    int ref[n], frame[f], i, j, k=0, fault=0, found;
+
+    srand(time(0));
+
+    for(i=0;i<n;i++){
+        ref[i]=rand()%10;
+        printf("%d ",ref[i]);
+    }
+
+    for(i=0;i<f;i++) frame[i]=-1;
+
+    for(i=0;i<n;i++){
+        found=0;
+        for(j=0;j<f;j++)
+            if(frame[j]==ref[i]) found=1;
+
+        if(!found){
+            frame[k]=ref[i];
+            k=(k+1)%f;
+            fault++;
+        }
+    }
+
+    printf("\nFIFO Page Faults: %d",fault);
+    return 0;
+}
